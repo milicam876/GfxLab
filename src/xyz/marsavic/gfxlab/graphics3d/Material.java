9a02686426b520ef9e205d503e26a6a18550c7d0
Marko Savić <4282607+marsavic@users.noreply.github.com>
2023-11-27 23:31:25 +0100

    Homework - RefractionTest scene.

--- src/xyz/marsavic/gfxlab/graphics3d/Material.java
+++ src/xyz/marsavic/gfxlab/graphics3d/Material.java
@@ -15,19 +15,23 @@ public record Material (
 	public Material specular       (Color  specular  ) { return new Material(diffuse, specular, shininess, reflective); }
 	public Material shininess      (double shininess ) { return new Material(diffuse, specular, shininess, reflective); }
 	public Material reflective     (Color  reflective) { return new Material(diffuse, specular, shininess, reflective);}
-
 	
-	public static final Material BLACK   = new Material(Color.BLACK, Color.BLACK, 1, Color.BLACK);
+	
+	// --- Utility constants and factory methods ---
+	
+	public static final Material BLACK   = new Material(Color.BLACK, Color.BLACK, 32, Color.BLACK);
 	
 	public static Material matte (Color  c) { return BLACK.diffuse(c); }
 	public static Material matte (double k) { return matte(Color.gray(k)); }
 	public static Material matte (        ) { return matte(1.0); }
 	public static final Material MATTE = matte();
 	
+	public static Material mirror(Color  c) { return BLACK.reflective(c); }
+	public static Material mirror(double k) { return mirror(Color.gray(k)); }
+	public static Material mirror(        ) { return mirror(1.0); }
+	public static final Material MIRROR = mirror();
 	
 	public static final Material DEFAULT = MATTE;
-	
-	public static final Material MIRROR = BLACK.reflective(Color.WHITE);
 	
 	
 	@Override
---
+++ src/xyz/marsavic/gfxlab/graphics3d/scenes/RefractionTest.java
@@ -0,0 +1,54 @@
+package xyz.marsavic.gfxlab.graphics3d.scenes;
+
+import xyz.marsavic.functions.F1;
+import xyz.marsavic.geometry.Vector;
+import xyz.marsavic.gfxlab.Color;
+import xyz.marsavic.gfxlab.Vec3;
+import xyz.marsavic.gfxlab.graphics3d.Light;
+import xyz.marsavic.gfxlab.graphics3d.Material;
+import xyz.marsavic.gfxlab.graphics3d.Scene;
+import xyz.marsavic.gfxlab.graphics3d.Solid;
+import xyz.marsavic.gfxlab.graphics3d.solids.Ball;
+import xyz.marsavic.gfxlab.graphics3d.solids.Group;
+import xyz.marsavic.gfxlab.graphics3d.solids.HalfSpace;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+
+public class RefractionTest extends Scene.Base {
+	
+	public RefractionTest() {
+		F1<Material, Vector> grid  = v -> Material.matte(                     v.add(Vector.xy(0.005)).mod(0.25).min() < 0.01 ? 0.5 : 1);
+		F1<Material, Vector> gridR = v -> Material.matte(Color.hsb(0   , 0.5, v.add(Vector.xy(0.005)).mod(0.25).min() < 0.01 ? 0.5 : 1));
+		F1<Material, Vector> gridG = v -> Material.matte(Color.hsb(0.33, 0.5, v.add(Vector.xy(0.005)).mod(0.25).min() < 0.01 ? 0.5 : 1));
+		
+		Collection<Solid> solids = new ArrayList<>();
+		Collections.addAll(solids,
+				HalfSpace.pn(Vec3.xyz(-1,  0,  0), Vec3.xyz( 1,  0,  0), gridG),
+				HalfSpace.pn(Vec3.xyz( 1,  0,  0), Vec3.xyz(-1,  0,  0), gridR),
+				HalfSpace.pn(Vec3.xyz( 0, -1,  0), Vec3.xyz( 0,  1,  0), grid),
+				HalfSpace.pn(Vec3.xyz( 0,  1,  0), Vec3.xyz( 0, -1,  0), grid),
+				HalfSpace.pn(Vec3.xyz( 0,  0,  1), Vec3.xyz( 0,  0, -1), grid)
+
+/*
+				TODO: Homework
+				Ball.cr(Vec3.xyz(-0.3,  0.3,  0.0), 0.4, uv -> Material.GLASS.refractive(Color.hsb(0.7, 0.2, 1.0))),
+				Ball.cr(Vec3.xyz( 0.4, -0.4,  0.0), 0.4, uv -> Material.GLASS),
+				Ball.cr(Vec3.xyz(-0.3, -0.4, -0.6), 0.4, uv -> Material.GLASS.refractiveIndex(2.5)),
+				Ball.cr(Vec3.xyz( 0.4,  0.3,  0.6), 0.4, uv -> Material.GLASS.refractiveIndex(0.6))
+*/
+		);
+		
+		Collections.addAll(lights,
+				Light.pc(Vec3.xyz(-0.7, 0.7, -0.7), Color.WHITE),
+				Light.pc(Vec3.xyz(-0.7, 0.7,  0.7), Color.WHITE),
+				Light.pc(Vec3.xyz( 0.7, 0.7, -0.7), Color.WHITE),
+				Light.pc(Vec3.xyz( 0.7, 0.7,  0.7), Color.WHITE)
+		);
+		
+		solid = Group.of(solids);
+	}
+	
+}
